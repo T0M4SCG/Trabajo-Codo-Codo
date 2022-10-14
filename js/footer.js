3663f7a353b222da7e3438a36ed4ac0303cc8a9a
T0M4SCG <tconchagiovana@udesa.edu.ar>
2022-10-14 13:17:41 -0300

    .

---
+++ js/footer.js
@@ -0,0 +1,10 @@
+let footer = document.querySelector("footer")
+
+footer.innerHTML = `
+<div>
+    <span class="bloqueFooter">
+        <div class="textoFooter"><a href="./index.html">HOME</a></div>
+        <div class="textoFooter"><a href="./playlist.html" >PLAYLIST</a></div>
+        <div class="textoFooter"><a href="./generos.html">GENEROS</a></div>
+        <div class="textoFooter"><a href="./contact.html">CONTACTANOS</a></div>
+</div>`
--- js/formulario.js
+++ js/formulario.js
@@ -1,27 +1,24 @@
 const nombre = document.getElementById("name")
 const email = document.getElementById("email")
 const password = document.getElementById("password")
-const pnombre = document.querySelector(".nombre")
-const pemail = document.querySelector(".email")
-const pcontraseña = document.querySelector(".contraseña")
+const advertencias = document.querySelector(".advertencias")
 const form = document.getElementById("form")
 
 form.addEventListener("submit", e =>{
-    e.preventDefault()
-    pnombre.innerHTML = ""
-    pemail.innerHTML = ""
-    pcontraseña.innerHTML = ""
+    advertencias.innerHTML = ""
     let validaremail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/
-    if (nombre.value.length < 6){
-        pnombre.innerHTML = `<p class="nombre">No has escrito un nombre valido</p>`
+    if (nombre.value.length < 3){
+        advertencias.innerHTML += `<p class="nombre">No has escrito un nombre valido</p>`
+        e.preventDefault()
     }
     if (!validaremail.test(email.value)){
-        pemail.innerHTML = `<p class="email">Escribe un mail valido</p>`
+        advertencias.innerHTML += `<p class="email">Escribe un mail valido</p>`
+        e.preventDefault()
     }
-    if (password.value.length < 8 || password.value.length > 16){
-        pcontraseña.innerHTML = `<p class="contraseña">Escriba una contraseña acorde</p>`
+    if (password.value.length < 8 || password.value.length > 30){
+        advertencias.innerHTML += `<p class="contraseña">Escriba una contraseña acorde</p>`
+        e.preventDefault()
     }
-    e.SubmitEvent
 })
 
 
